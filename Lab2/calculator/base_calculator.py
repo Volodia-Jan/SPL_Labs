6664d8d333342ea92c2fe0af31df24b920fb3894
Volodymyr Hula <volodia.gula@ukr.net>
2023-10-10 16:20:23 +0300

    Implemented task 5

--- Lab2/calculator/base_calculator.py
+++ Lab2/calculator/base_calculator.py
@@ -6,6 +6,16 @@ class BaseCalculator:
         self.__first_number = None
         self.__valid_operators = valid_operators
 
+    def do_operation(self):
+        if self.__operator == '+':
+            return self.__first_number + self.__second_number
+        elif self.__operator == '-':
+            return self.__first_number - self.__second_number
+        elif self.__operator == '*':
+            return self.__first_number * self.__second_number
+        elif self.__operator == '/':
+            return self.__first_number / self.__second_number
+
     def user_input(self):
         self.__first_number = int(input("Enter first number"))
         self.__second_number = int(input("Enter second number"))
