adb4217c081566a87397d79487d1752ebb322247
Volodymyr Hula <volodia.gula@ukr.net>
2023-10-17 12:17:52 +0300

    Implemented 2-3 tasks of 3th lab

--- Lab3/main.py
+++ Lab3/main.py
@@ -2,8 +2,15 @@ import pyfiglet
 
 
 def main():
-    user_input = input("Введіть слово або фразу: ")
-    ascii_art = pyfiglet.figlet_format(user_input)
+    user_input = input("Enter a word or phrase to convert to ASCII art: ")
+    print("User input:", user_input)
+    font_list = pyfiglet.FigletFont.getFonts()
+    print("Available fonts:")
+    for font in font_list:
+        print(font)
+
+    font_choice = input("Choose a font from the list above: ")
+    ascii_art = pyfiglet.figlet_format(user_input, font=font_choice)
     print(ascii_art)
 
 
